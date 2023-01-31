aa02256658837f0084dc810ebc4b03ba7e5cd3c0
Wa-lead <waleedaaofficial@gmail.com>
2023-01-31 22:24:59 +0300

    final

--- app/main.py
+++ app/main.py
@@ -2,37 +2,28 @@ from fastapi import FastAPI, File, UploadFile
 import mysql.connector
 
 
+columns = ['name', 'id', 'degree', 'field_of_study', 'university', 'graduation_year', 'courses',
+           'health_history', 'intersts', 'car', 'house', 'job', 'city', 'country', 'date_of_birth']
 
-import pandas as pd
 
 app = FastAPI()
 
-@app.get("/dataframe")
-async def get_dataframe():
-    cnx = mysql.connector.connect(
-    host="rm-l4vtsmuu203976dh4qo.mysql.me-central-1.rds.aliyuncs.com",
-    user="admin_account",
-    password="Admin@2023",
-    database="mysql"
-    )
 
-    cursor = cnx.cursor()
+@app.get("/data")
+async def return_data(id):
+        cnx = mysql.connector.connect(
+                host="rm-l4vtsmuu203976dh4qo.mysql.me-central-1.rds.aliyuncs.com",
+                user="admin_account",
+                password="Admin@2023",
+                database="mysql"
+        )
 
-    query = """SELECT 
-            name,
-            id,
-            degree,
-            field_of_study,
-            university,
-            graduation_year,
-            courses
-    FROM new_schema.ayman
-    """
+        cursor = cnx.cursor()
+        print()
+        query = f"SELECT * FROM new_schema.data2 WHERE id = \"{id}\""
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cnx.close()
+        return dict(zip(columns, result[0]))
 
-    cursor.execute(query)
 
-    result = cursor.fetchall()
-
-    cnx.close()
-    df = pd.read_json("data.json")
-    return df.to_json(orient='records')
