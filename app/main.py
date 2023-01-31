cede91166acab4ad97b5ae3a8404856da9f4f58b
Waleed <81301826+Wa-lead@users.noreply.github.com>
2023-01-31 22:02:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,38 @@
 from fastapi import FastAPI, File, UploadFile
+import mysql.connector
+
+
+
 import pandas as pd
 
 app = FastAPI()
 
 @app.get("/dataframe")
 async def get_dataframe():
+    cnx = mysql.connector.connect(
+    host="rm-l4vtsmuu203976dh4qo.mysql.me-central-1.rds.aliyuncs.com",
+    user="admin_account",
+    password="Admin@2023",
+    database="mysql"
+    )
+
+    cursor = cnx.cursor()
+
+    query = """SELECT 
+            name,
+            id,
+            degree,
+            field_of_study,
+            university,
+            graduation_year,
+            courses
+    FROM new_schema.ayman
+    """
+
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+
+    cnx.close()
     df = pd.read_json("data.json")
     return df.to_json(orient='records')
-
