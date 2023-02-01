45d2c8730ee1e4ec33ea231d8521ce00f1cd7dc5
Waleed <81301826+Wa-lead@users.noreply.github.com>
2023-02-01 16:39:01 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 import mysql.connector
+from flask import Flask, request
+import numpy as np
+
+cdt_columns = ['name', 'id', 'degree', 'field_of_study', 'university', 'graduation_year', 'courses',
+           'health_history', 'intersts', 'car', 'house', 'job', 'city', 'country', 'date_of_birth', 
+           'family_members', 'company_coords']
 
 
-columns = ['name', 'id', 'degree', 'field_of_study', 'university', 'graduation_year', 'courses',
-           'health_history', 'intersts', 'car', 'house', 'job', 'city', 'country', 'date_of_birth']
+app = Flask(__name__)
 
 
-app = FastAPI()
-
-
-@app.get("/data")
-async def return_data(id):
+@app.route('/', methods=['GET'])
+def return_data():
+        id = request.args.get('id')
         cnx = mysql.connector.connect(
                 host="rm-l4vtsmuu203976dh4qo.mysql.me-central-1.rds.aliyuncs.com",
                 user="admin_account",
@@ -19,11 +22,17 @@ async def return_data(id):
         )
 
         cursor = cnx.cursor()
-        print()
-        query = f"SELECT * FROM new_schema.data2 WHERE id = \"{id}\""
+        query = f"SELECT * FROM new_schema.data WHERE id = \"{id}\""
         cursor.execute(query)
         result = cursor.fetchall()
         cnx.close()
-        return dict(zip(columns, result[0]))
+        result= dict(zip(cdt_columns, np.array(result[0]).T))
+        result['intersts'] = eval(result['intersts'])
+        result['courses'] = eval(result['courses'])
+        return result
+        
+        
 
+if __name__ == '__main__':
+        app.run(host='127.0.0.1', port=80, debug=True)
 
